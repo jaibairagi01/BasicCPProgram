e400ad943c4fe9e54fb5ddfb71a3310cf121ffee
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2023-03-12 00:59:25 +0530

    Add files via upload

---
+++ AtoZfor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char i;
+
+    cout << "The Alphabets from A to Z are: ";
+
+    for (i = 'A'; i <= 'Z'; i++)
+    {
+        cout << i << " ";
+    }
+
+    cout << "The Alphabets from a to z are: ";
+
+    for (i = 'a'; i <= 'z'; i++)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
