27ba6459f30ba5d10e4773dce0320e6ff1664361
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2023-03-12 01:17:30 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter the no: \n";
+    cin >> n;
+
+    if (n % 2 == 0)
+    {
+        cout << n << " is even ";
+    }
+    else
+    {
+        cout << n << " is odd ";
+    }
+    return 0;
+}
